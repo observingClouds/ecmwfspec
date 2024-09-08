aeab67de15892abba3abf7e7bb03f43659daf965
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-09-08 18:39:18 +0200

    switch to pyproject.toml

--- ecmwfspec/__init__.py
+++ ecmwfspec/__init__.py
@@ -1,10 +1,7 @@
 from fsspec import register_implementation
 
-from . import _version
 from .core import ECFileSystem, logger
-
-__version__ = _version.get_versions()["version"]
 
 register_implementation(ECFileSystem.protocol, ECFileSystem)
 
-__all__ = ["__version__", "ECFileSystem", "logger"]
+__all__ = ["ECFileSystem", "logger"]

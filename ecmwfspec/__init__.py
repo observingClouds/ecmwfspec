00035d1cb590b338bc83b118b3cdddf1a5093fc6
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-09-15 08:11:39 +0000

    add missing registration for ectmp proto

--- ecmwfspec/__init__.py
+++ ecmwfspec/__init__.py
@@ -2,6 +2,8 @@ from fsspec import register_implementation
 
 from .core import ECFileSystem, ECTmpFileSystem, logger
 
-register_implementation(ECFileSystem.protocol, ECFileSystem, ECTmpFileSystem)
+register_implementation(
+    ECFileSystem.protocol, ECFileSystem, ECTmpFileSystem, ECTmpFileSystem.protocol
+)
 
 __all__ = ["ECFileSystem", "ECTmpFileSystem", "logger"]
